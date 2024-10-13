9ade3f473f2b85f5548c6601513f77a85230fc3b
fabricioescobar9 <esc24001@byui.edu>
2024-10-13 02:56:44 -0300

    Week 2 Assignment - Finished

--- scripts/temples.js
+++ scripts/temples.js
@@ -13,8 +13,10 @@ document.getElementById("lastModified").innerHTML = `Last Modification: ${lastMo
 
 const barMenu = document.querySelector(".navigation");
 const hamburgerButton = document.querySelector("#menu");
+const mainTitle = document.querySelector("maintitle");
 
 hamburgerButton.addEventListener("click", () => {
     barMenu.classList.toggle("show");
     hamburgerButton.classList.toggle("show");
+    mainTitle.classList.toogle("maintitle");
 });
