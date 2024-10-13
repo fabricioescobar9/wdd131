0874b3200692c28f8c28ec09f98e928f273e2d94
fabricioescobar9 <esc24001@byui.edu>
2024-10-13 02:05:21 -0300

    Week 2 Assignment - Updated

---
+++ scripts/temples.js
@@ -0,0 +1,20 @@
+// This get the current year (In this case 2024)
+
+const currentyear = document.querySelector("#currentyear");
+
+const today = new Date();
+
+// This get the last modification in the code
+
+currentyear.innerHTML = `©${today.getFullYear()}`;
+
+let lastModified = document.lastModified;
+document.getElementById("lastModified").innerHTML = `Last Modification: ${lastModified}`;
+
+const barMenu = document.querySelector(".navigation");
+const hamburgerButton = document.querySelector("#menu");
+
+hamburgerButton.addEventListener("click", () => {
+    barMenu.classList.toggle("show");
+    hamburgerButton.classList.toggle("show");
+});
