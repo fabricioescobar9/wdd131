d56b9d8c1a610195884b1e63a48807de12f5bd01
fabricioescobar9 <fescobarcruz@byupathway.edu>
2026-03-19 01:51:32 -0300

    Week 02 Activity BOM Completed!

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,25 +2,23 @@ const input = document.querySelector("#favchap");
 const button = document.querySelector("button");
 const list = document.querySelector("#list");
 
-const li = document.createElement("li");
-const delBtn = document.createElement("button");
-
-li.text = 
-
 button.addEventListener("click", function() {
     if (input.value.trim() !== "") {
+        const li = document.createElement("li");
         li.textContent = input.value;
+
+        const delBtn = document.createElement("button");
         delBtn.textContent = "❌";
 
-        li.append(delBtn);
-        list.append(li);
+        delBtn.addEventListener("click", function() {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        li.appendChild(delBtn);
+        list.appendChild(li);
+        input.value = "";
     }
-})
 
-delBtn.addEventListener("click", function() {
-    list.removeChild(li);
-    input.focus();
-})
-
-input.value = ""
-input.focus();
+	input.focus();
+});
